da05eb179a1bd3e682a6c2a951ebb13708c21050
Théo Grébeude <theogrebeude@gmail.com>
2018-04-27 10:37:18 +0200

    Create eval.groovy

---
+++ eval.groovy
@@ -0,0 +1,15 @@
+job('Sample PHP Site V1') {
+    description 'Site PHP en V1'
+    scm {
+        git('https://gitlab.com/alexandre.morat/eval-project.git'){node ->
+            node / gitConfigName('theo')
+            node / gitConfigEmail('theo@theo.com')        
+        }
+    }
+    triggers{
+        scm('H/15 * * * *')
+    }
+     wrappers {
+        php
+    }    
+}
