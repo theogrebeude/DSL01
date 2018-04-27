0fd213aaaa2cfb0fe425ed787d8a435815082528
Théo Grébeude <theogrebeude@gmail.com>
2018-04-27 11:02:15 +0200

    Update eval.groovy

--- eval.groovy
+++ eval.groovy
@@ -14,3 +14,37 @@ job('Sample PHP Site V1') {
         scm('H/15 * * * *')
     }  
 }
+
+job('Sample PHP Site V2') {
+    description 'Site PHP en V2'
+    scm {
+        git {
+            remote {
+                    name('origin')
+                    url('git@gitlab.com:alexandre.morat/eval-project.git')
+                }
+                branch('v2')
+            }
+        }
+
+    triggers{
+        scm('H/15 * * * *')
+    }  
+}
+
+job('Sample PHP Site V3') {
+    description 'Site PHP en V3'
+    scm {
+        git {
+            remote {
+                    name('origin')
+                    url('git@gitlab.com:alexandre.morat/eval-project.git')
+                }
+                branch('v3')
+            }
+        }
+
+    triggers{
+        scm('H/15 * * * *')
+    }  
+}
